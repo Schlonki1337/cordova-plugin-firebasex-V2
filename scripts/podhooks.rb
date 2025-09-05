4ccc0ff55ba6da47724c52f665198c71d1f0662a
Schlonki123 <steffen.von.thienen@gmail.com>
2025-09-05 17:14:03 +0200

    Update podhooks.rb

--- scripts/podhooks.rb
+++ scripts/podhooks.rb
@@ -1,15 +1,17 @@
 post_install do |installer|
   installer.pods_project.targets.each do |target|
     target.build_configurations.each do |config|
-      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '12.0'
+      # Existing fixes
       config.build_settings['EXCLUDED_ARCHS[sdk=iphonesimulator*]'] = 'arm64'
+      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '12.0'
 
-      if target.name == 'gRPC-C++' || target.name == 'BoringSSL-GRPC'
+      # Disable Swift optimization to avoid errors
+      config.build_settings['SWIFT_OPTIMIZATION_LEVEL'] = '-Onone'
+
+      # Optional: Only for BoringSSL-GRPC
+      if target.name == 'BoringSSL-GRPC'
         config.build_settings['CLANG_CXX_LANGUAGE_STANDARD'] = 'c++17'
         config.build_settings['CLANG_CXX_LIBRARY'] = 'libc++'
-        if config.build_settings['OTHER_CFLAGS']
-          config.build_settings['OTHER_CFLAGS'] = config.build_settings['OTHER_CFLAGS'].gsub('-G', '')
-        end
       end
     end
   end
